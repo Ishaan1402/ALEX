a73a8baab8839c0b31758e6868deabe879d3be6b
John Doe <johndoe@example.com>
2023-03-31 13:30:36 -0400

    Edited print statement

--- src/benchmark/main.cpp
+++ src/benchmark/main.cpp
@@ -166,7 +166,7 @@ int main(int argc, char* argv[]) {
                 << index.num_nodes()
                 << "\tnumber of data nodes:\t"
                 << index.num_leaves()
-                << "\nnumber of model nodes:\t"
+                << "\n\tnumber of model nodes:\t"
                 << index.stats_.num_model_nodes
                 << std::endl;
     }
